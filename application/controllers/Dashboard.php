8348e4652b268fadace5fe99332460da615542c8
rhonbhorj <rhonbhorj@gmail.com>
2024-05-20 16:16:55 +0800

    adding dashboard controller

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Dashboard_repo', "repo");
+    }
+
+    public function index()
+    {
+        echo "pangit";
+    }
+
+    public function dasboardRepoertDdata()
+    {
+        $data['alltransaction'] = $this->repo->all_transaction_data();
+        $data['today-transaction'] = $this->repo->all_transaction_today();
+
+        $today = date('l');
+
+        // Initialize the $yesterday array
+        $yesterday = array();
+
+        // Switch case to handle different days of the week
+        switch ($today) {
+            case 'Monday':
+                // Only today's date
+                // $i = 0;
+                // $yesterday[] = date('Y-m-d', strtotime("days", strtotime($today)));
+
+                for ($i = 0; $i < 5; $i ++) {
+                    $yesterday[] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Tuesday':
+
+                for ($i = 0; $i < 2; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Wednesday':
+                for ($i = 0; $i < 3; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Thursday':
+
+                for ($i = 0; $i < 4; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Friday':
+
+                for ($i = 0; $i < 5; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Saturday':
+
+                for ($i = 0; $i < 6; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            case 'Sunday':
+
+                for ($i = 0; $i < 7; $i ++) {
+                    $yesterday['data' . ($i + 1)] = date('Y-m-d', strtotime("-$i days", strtotime($today)));
+                }
+                break;
+
+            default:
+                // Default case to handle any unexpected input
+                $yesterday['data'] = $today;
+                break;
+        }
+
+        $data['all-transaction-this-week'] = $this->repo->all_transaction_this_week($yesterday, $i);
+
+        echo json_encode($data);
+
+        // dito pert
+        // $this->load->view('<your dashbaord view', $data);
+    }
+}
---
+++ application/models/Dashboard_repo.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard_repo extends CI_Model
+{
+
+    public function all_transaction_data()
+    {
+        $result = "SELECT  SUM(txn_amount) AS total_txn_amount ,count(txn_amount) as total_count 
+                    FROM transactions  where  status='SUCCESS'";
+
+        $data = $this->db->query($result);
+        return $data->row_array() ? $data->row_array() : false;
+    }
+
+    public function all_transaction_today()
+    {
+        date_default_timezone_set('Asia/Manila');
+        $today = date('Y-m-d');
+        $result = "SELECT  SUM(txn_amount) AS total_txn_amount_total ,count(txn_amount) as total_count_totay 
+                  FROM transactions  where   last_modified like '%" . $today . "%'";
+
+        $data = $this->db->query($result);
+        return $data->row_array() ? $data->row_array() : false;
+    }
+
+    public function all_transaction_yesterday()
+    {
+        date_default_timezone_set('Asia/Manila');
+        $today = date('Y-m-d');
+
+        $yesterday = date('Y-m-d', strtotime('-1 day', strtotime($today)));
+
+        $result = "SELECT  SUM(txn_amount) AS total_txn_amount_yesterday ,count(txn_amount) as total_count_yesterday
+                   FROM transactions  where    like '%" . $yesterday . "%'";
+
+        $data = $this->db->query($result);
+        return $data->row_array() ? $data->row_array() : false;
+    }
+
+    public function all_transaction_this_week($datesArray, $count)
+    {
+        $today = date('Y-m-d');
+
+        $dates = array_values($datesArray);
+
+        $likePatterns = array_map(function ($date) {
+            return " '%" . $date . "%'";
+        }, $dates);
+
+        for ($i = 0; $i < $count; $i ++) {
+
+            $sql = "SELECT count(txn_amount) as total_count FROM transactions where  last_modified like " . $likePatterns[$i] . " and status='SUCCESS'";
+
+            $data = $this->db->query($sql);
+
+            $resultArray[$i] = $data->num_rows() > 0 ? $data->row_array() : false;
+
+        
+        }
+
+        $dayresult = [];
+
+        foreach ($datesArray as $date) {
+            $timestamp = strtotime($date);
+            $dayOfWeek = date('l', $timestamp);
+            $dayresult[] = $dayOfWeek;
+        }
+
+        $result = [];
+
+        foreach ($dayresult as $index => $day) {
+            $result[$day] = $resultArray[$index];
+        }
+
+        return $result;
+    }
+    public function  monthly_transaction()
+    {
+        
+    }
+}
