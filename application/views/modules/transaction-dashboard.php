cc08b2e6d1f3de9e09b293edb2aa90168296af16
Paul Cullen <32891588+pauuuuuuuuuu1@users.noreply.github.com>
2024-05-29 16:06:58 +0800

    add combo chart in dashboard

--- application/views/modules/transaction-dashboard.php
+++ application/views/modules/transaction-dashboard.php
@@ -23,29 +23,33 @@
     background: #FAFAFA;
     box-shadow: 2px 2px 5px 0 #DDD;
   }
+
   .dashboard-container {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
   }
-/*  */
+
+  /*  */
 
   .report-inner-cards-wrapper {
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
   }
-  span.report-title{
-    color: #FFF!important;
+
+  span.report-title {
+    color: #FFF !important;
     margin-bottom: 20px;
     font-size: 16px !important;
   }
+
   /* .h4{
     margin-top: -22px !important;
   } */
   .report-inner-card {
     background: #00507A;
-    height:130px;
+    height: 130px;
     flex: 1;
     margin: 0.5rem;
     padding: 1rem;
@@ -87,7 +91,10 @@
     background-color: #DC3545;
   }
 
-  #bar-chart-daily, #line-chart-monthly,#bar-chart-daily-count, #line-chart-monthly-count {
+  #bar-chart-daily,
+  #line-chart-monthly,
+  #bar-chart-daily-count,
+  #line-chart-monthly-count {
     width: 100%;
     height: 300px;
   }
@@ -104,9 +111,8 @@
       <div class="row">
         <div class="col-md-12 grid-margin">
           <div class="card">
-            
             <div class="card-body">
-            <H4>Total Transaction Amount</H4>
+              <H4>Total Transaction Amount</H4>
               <div class="row report-inner-cards-wrapper">
                 <div class="col-md-6 col-xl report-inner-card ">
                   <div class="inner-card-text">
@@ -145,19 +151,25 @@
       </div>
     </section>
     <section>
-    <section>
-      <div class="row">
-        <div class="col-md-6">
-          <h5>Daily Page Hits</h5>
-          <div id="bar-chart-daily"></div>
+      <section>
+        <div class="card-deck">
+          <div class="card">
+            <div class="card-body">
+              <h5 style="margin-left: 5px;margin-bottom: 20px;">Daily Page Hits</h5>
+              <div id="bar-chart-daily"></div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <h5 style="margin-bottom: 20px;">Monthly Page Hits</h5>
+              <div id="line-chart-monthly"></div>
+            </div>
+
+          </div>
         </div>
-        <div class="col-md-6">
-          <h5>Monthly Page Hits</h5>
-          <div id="line-chart-monthly"></div>
-        </div>
-      </div>
-    </section>
-     
+
+      </section>
+
     </section>
 
     <section>
@@ -165,7 +177,7 @@
         <div class="col-md-12 grid-margin">
           <div class="card">
             <div class="card-body">
-            <H4>Total Transaction Count (Success, Failed)</H4>
+              <H4>Total Transaction Count (Success, Failed)</H4>
               <div class="row report-inner-cards-wrapper">
                 <div class="col-md-6 col-xl report-inner-card ">
                   <div class="inner-card-text">
@@ -200,19 +212,20 @@
         </div>
       </div>
     </section>
-
-
-<!-- amount graph -->
-
-<section>
-      <div class="row">
-        <div class="col-md-6">
-          <h5>Daily Page Hits</h5>
-          <div id="bar-chart-daily-count"></div>
+    <!-- amount graph -->
+    <section>
+      <div class="card-deck">
+        <div class="card">
+          <div class="card-body">
+            <h5>Daily Transaction Hits</h5>
+            <div id="bar-chart-daily-count" style="margin-left: -25px;"></div>
+          </div>
         </div>
-        <div class="col-md-6">
-          <h5>Monthly Page Hits</h5>
-          <div id="line-chart-monthly-count"></div>
+        <div class="card">
+          <div class="card-body">
+            <h5>Monthly Transaction Hits</h5>
+            <div id="line-chart-monthly-count"></div>
+          </div>
         </div>
       </div>
     </section>
@@ -221,7 +234,7 @@
 <script src="https://www.gstatic.com/charts/loader.js"></script>
 
 <script>
-  document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', function() {
     const url = '<?php echo base_url() ?>/TransactionReport/dasboardReportData';
 
     fetch(url)
@@ -254,12 +267,19 @@
 
           // Bar graph for Daily Total Txn Amount
           const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-          const dailyDataArray = [['Day', 'Success', { role: 'tooltip', 'p': {'html': true}}]];
+          const dailyDataArray = [
+            ['Day', 'Success', {
+              role: 'tooltip',
+              'p': {
+                'html': true
+              }
+            }]
+          ];
 
           daysOfWeek.forEach(day => {
             dailyDataArray.push([
-              day.slice(0, 3), 
-              parseFloat((responseData.all_transaction_this_week[day]?.total_txn_amount ?? '0').replace(/,/g, '')), 
+              day.slice(0, 3),
+              parseFloat((responseData.all_transaction_this_week[day]?.total_txn_amount ?? '0').replace(/,/g, '')),
               createCustomTooltip(responseData.all_transaction_this_week[day])
             ]);
           });
@@ -267,13 +287,20 @@
           var dailyData = google.visualization.arrayToDataTable(dailyDataArray);
 
           // Line graph for Monthly Total Txn Amount
-          const monthOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November','December'];
-          const monthlyDataArray = [['Day', 'Success', { role: 'tooltip', 'p': {'html': true}}]];
+          const monthOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+          const monthlyDataArray = [
+            ['Day', 'Success', {
+              role: 'tooltip',
+              'p': {
+                'html': true
+              }
+            }]
+          ];
 
           monthOfYear.forEach(month => {
             monthlyDataArray.push([
-              month.slice(0, 3), 
-              parseFloat((responseData.monthly_transaction[month]?.total_txn_amount ?? '0').replace(/,/g, '')), 
+              month.slice(0, 3),
+              parseFloat((responseData.monthly_transaction[month]?.total_txn_amount ?? '0').replace(/,/g, '')),
               createCustomTooltip(responseData.monthly_transaction[month])
             ]);
           });
@@ -282,11 +309,13 @@
 
           // Bar graph for Daily Total Txn Count Success & Failed
           const daysOfWeekCount = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-          const dailyDataArrayCount = [['Day', 'Success', 'Failed']];
+          const dailyDataArrayCount = [
+            ['Day', 'Success', 'Failed']
+          ];
 
           daysOfWeekCount.forEach(day => {
             dailyDataArrayCount.push([
-              day.slice(0, 3), 
+              day.slice(0, 3),
               responseData.all_transaction_this_week[day]?.total_count ?? '0',
               responseData.all_transaction_this_week[day]?.total_count_failed ?? '0'
             ]);
@@ -294,13 +323,15 @@
 
           var dailyData_count = google.visualization.arrayToDataTable(dailyDataArrayCount);
 
-           // Line graph for Daily Total Txn Count Success & Failed
-          const monthOfYearCount = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November','December'];
-          const monthlyDataArrayCount = [['Day', 'Success', 'Failed']];
+          // Line graph for Daily Total Txn Count Success & Failed
+          const monthOfYearCount = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+          const monthlyDataArrayCount = [
+            ['Day', 'Success', 'Failed']
+          ];
 
           monthOfYearCount.forEach(month => {
             monthlyDataArrayCount.push([
-              month.slice(0, 3), 
+              month.slice(0, 3),
               responseData.monthly_transaction[month]?.total_count ?? '0',
               responseData.monthly_transaction[month]?.total_count_failed ?? '0'
             ]);
@@ -309,7 +340,7 @@
           var monthlyData_count = google.visualization.arrayToDataTable(monthlyDataArrayCount);
 
 
-          
+
 
           var barOptions = {
             backgroundColor: 'transparent',
@@ -350,7 +381,9 @@
               duration: 1200,
               easing: 'out'
             },
-            tooltip: { isHtml: true }
+            tooltip: {
+              isHtml: true
+            }
           };
 
           var lineOptions = {
@@ -391,20 +424,68 @@
             },
             lineWidth: 2,
             pointSize: 5,
-            tooltip: { isHtml: true }
+            tooltip: {
+              isHtml: true
+            }
+          };
+
+            var options = {
+            backgroundColor: 'transparent',
+            colors: ['#00507A', '#f08078'],
+            fontName: 'Open Sans',
+            chartArea: {
+              left: 50,
+              top: 10,
+              width: '100%',
+              height: '70%'
+            },
+            bar: {
+              groupWidth: '70%'
+            },
+            hAxis: {
+              textStyle: {
+                fontSize: 11
+              }
+            },
+            vAxis: {
+              minValue: 0,
+              baselineColor: '#6ad4cd',
+              gridlines: {
+                color: '#6ad4cd',
+                count: 4
+              },
+              textStyle: {
+                fontSize: 11
+              }
+            },
+            legend: {
+              position: 'bottom',
+              textStyle: {
+                fontSize: 12
+              }
+            },
+            animation: {
+              duration: 1200,
+              easing: 'out'
+            },
+            tooltip: {
+              isHtml: true
+            },
+            series: {1: {type: 'line'}},
+            lineWidth: 3
           };
 
           var dailyChart = new google.visualization.ColumnChart(document.getElementById('bar-chart-daily'));
-          dailyChart.draw(dailyData, barOptions);
+          dailyChart.draw(dailyData_count, options);
 
-          var monthlyChart = new google.visualization.LineChart(document.getElementById('line-chart-monthly'));
-          monthlyChart.draw(monthlyData, lineOptions);
+          var monthlyChart = new google.visualization.ColumnChart(document.getElementById('line-chart-monthly'));
+          monthlyChart.draw(monthlyData_count, options);
 
           var dailyChart_count = new google.visualization.ColumnChart(document.getElementById('bar-chart-daily-count'));
           dailyChart_count.draw(dailyData_count, barOptions);
 
-          var monthlyChart_count = new google.visualization.LineChart(document.getElementById('line-chart-monthly-count'));
-          monthlyChart_count.draw(monthlyData_count, lineOptions);
+          var monthlyChart_count = new google.visualization.ColumnChart(document.getElementById('line-chart-monthly-count'));
+          monthlyChart_count.draw(monthlyData_count, barOptions);
 
         }
 
@@ -425,5 +506,6 @@
         console.error('Error fetching the report data:', error);
       });
   });
-  
+
+  git
 </script>
