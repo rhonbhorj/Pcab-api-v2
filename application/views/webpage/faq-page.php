c88390b7ca373d18601f6be838657c9451809915
michael angelo garcia <garciacruzmichaelangelo@gmail.com>
2024-06-18 15:14:52 +0800

    added faqs

--- application/views/webpage/faq-page.php
+++ application/views/webpage/faq-page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,24 +13,25 @@
 
 
 </head>
+
 <body>
     <section class="main">
         <section class="pages" id="home" data-type="background" data-speed="10">
-            <div class="logo"  >
-            <img src='<?php echo base_url("/assets/images/webpage/logo.png") ?>'>
+            <div class="logo">
+                <img src='<?php echo base_url("/assets/images/webpage/logo.png") ?>'>
             </div>
         </section>
-        
+
         <section class="pages" id="about" data-type="background">
-           
+
             <div class="cinema-mode">
                 <h1>PAY YOUR PCAB FEES via QRPh</h1>
                 <video autoplay muted controls class="pcabvideo">
                     <source src='<?php echo base_url("/assets/images/webpage/finalpcab.mp4") ?>' type="video/mp4">
                 </video>
             </div>
-</section>
-        
+        </section>
+
         <section class="pages" id="howto" data-type="background">
             <div class="carousel">
                 <div class="carousel-inner">
@@ -39,41 +41,41 @@
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step1.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step1.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step2.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step2.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-4" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step3.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step3.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-5" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step4.1.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step4.1.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-6" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step4.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step4.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-7" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step5.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step5.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-8" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step6.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step6.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-9" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step7.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step7.png") ?>'>
                     </div>
                     <input class="carousel-open" type="radio" id="carousel-10" name="carousel" aria-hidden="true" hidden="">
                     <div class="carousel-item">
-                    <img src='<?php echo base_url("/assets/images/webpage/Step8.png") ?>'>
+                        <img src='<?php echo base_url("/assets/images/webpage/Step8.png") ?>'>
                     </div>
-                    
+
                     <label for="carousel-10" class="carousel-control prev control-1">‹</label>
                     <label for="carousel-2" class="carousel-control next control-1">›</label>
                     <label for="carousel-1" class="carousel-control prev control-2">‹</label>
@@ -98,54 +100,162 @@
                 </div>
             </div>
             <div class="left">
-                <p class="paragraph">Download the instructional guide on how to pay</p> 
+                <p class="paragraph">Download the instructional guide on how to pay</p>
                 <p class="paragraph">your PCAB Fees via QRPh</p>
                 <a href="https://drive.google.com/file/d/1HPB6DCLcf52egN5qeVoW4ONqMSe03CjG/view?usp=drive_link" style="text-decoration: none" target="_blank"><button class="download"></button></a>
             </div>
         </section>
-        
+
         <section class="pages" id="survey" data-type="background">
             <div class="container">
                 <div class="surveyheading">
-                <h1>HELP US OUT!</h1>
-            </div>
+                    <h1>HELP US OUT!</h1>
+                </div>
                 <p>In the effort of improving our services to meet your needs, please take a few minutes to complete the <b>survey form.</b> Your input will not only help us refine our process, but also enable us to provide you with an even better experience.</p>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSfOloGNfAmNGWCvdbUaCiWQ_tc8q7Z9Ep48BeEBfMmiXMmTHw/viewform" target="_blank"><button class="survey"></button>
-                </a></div>            
+                </a>
+            </div>
         </section>
-        
-<!--                 
-        <section class="pages" id="faqs" data-type="background">
-   <div class="faqsheading" style="justify-content: center;padding: 10px; align-items: center;">
-    <h1>FREQUENTLY ASK QUESTIONS</h1>    
-</div>
-        </section> -->
-        
+        <section class="pages" id="faqbody" data-type="background">
+            <div class="container1 newfaq">
+                <section>
+                    <div class="title">Frequently Ask Question</div>
+                    <p>
+                        To help you make your experience easier, we've compiled our most
+                        <b>frequently asked questions</b> and answers below.
+                    </p>
+                </section>
+                <section class="accordion">
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd1" />
+                        <label for="rd1" class="tab__label">
+                            Can I pay QRPH using credit card ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Currently, we don’t facilitate QRPH payments via credit cards but will
+                                be soon available. You may pay thru QRPH with real-time cash payments
+                                using your E-Wallet and Mobile Banking.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd2" />
+                        <label for="rd2" class="tab__label">
+                            What are the E-wallets and mobile banks that I can use to pay QRPH ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd3" />
+                        <label for="rd3" class="tab__label">
+                            Why does my e-wallet say "insufficient credit limit" when I have enough
+                            balance? What should I do ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd4" />
+                        <label for="rd4" class="tab__label">
+                            What are the e-wallets or mobile banks that can customize the wallet
+                            limit ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd5" />
+                        <label for="rd5" class="tab__label">
+                            Can I pay the fees partially using QRPH ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd6" />
+                        <label for="rd6" class="tab__label">
+                            Will I be receiving receipt after the payment? Where and when will i
+                            receive it ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="tab">
+                        <input type="radio" name="q-accordion" id="rd7" />
+                        <label for="rd7" class="tab__label">
+                            I am having trouble after scanning the QR code. What do I do ?
+                        </label>
+                        <div class="tab__content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum id
+                                impedit expedita quaerat quisquam repellat sint quos nemo error autem,
+                                harum iusto eum. Nemo asperiores, odio sequi quidem molestiae
+                                voluptates!
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+
         <footer class="footer">
             <div class="footer-content">
                 <div class="logo-container">
                     <img src='<?php echo base_url("/assets/images/webpage/PCAB logo.png") ?>' alt="PCAB">
                     <img src='<?php echo base_url("/assets/images/webpage/NGSI LOGO.png") ?>' alt="NGSI">
-          
+
                 </div>
 
 
-            
-            
+
+
                 <!-- <div class="footer-connect">
                     <h1 >Connect with us <br>to learn more!</h1>
                 </div> -->
-                <div class="footer-details"style="line-height: 40px!important;">    
+                <div class="footer-details" style="line-height: 40px!important;">
                     <h1 style="margin-bottom: 10px;-webkit-text-stroke: 1px;">HAVING ISSUES? CONTACT US:</h1>
                     <p><i class="fa fa-map-marker" aria-hidden="true"></i> Sentro Kapitolyo Bldg. 59 West Capitol Drive St., corner Stella Maris, Brgy. Kapitolyo, Pasig, 1603</p>
                     <p><i class="fa fa-globe"></i><a href="https://netglobalsolutions.net" target="_blank"> https://netglobalsolutions.net</p></a>
                     <p><i class="fa fa-facebook"></i><a href="https://facebook.com/netglobalsolutionsinc" target="_blank"> https://facebook.com/netglobalsolutionsinc</p></a>
-                  
-                   
-                
+
+
+
                 </div>
-                <div class="footer-details"style="line-height: 40px!important;">
-                        
+
+                <div class="footer-details" style="line-height: 40px!important;">
+
                     <p><i class="fa fa-envelope-o"></i> @supportnetglobalsolutions.net</p>
                     <p><i class="fa fa-phone" aria-hidden="true"></i> (02) 82877374</p>
                     <p><i class="fa fa-mobile" aria-hidden="true"></i> 09171486979 / 09171793481 / 09173126960</p>
@@ -153,13 +263,14 @@
             </div>
             </div>
         </footer>
-   
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src='<?php echo base_url("/assets/js/webpage/webpage.js") ?>'></script> 
-    <script src='<?php echo base_url("/assets/js/webpage/slider.js") ?>'></script> 
 
-    
-  
-    <!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src='<?php echo base_url("/assets/js/webpage/webpage.js") ?>'></script>
+        <script src='<?php echo base_url("/assets/js/webpage/slider.js") ?>'></script>
+
+
+
+        <!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
 </body>
+
 </html>
